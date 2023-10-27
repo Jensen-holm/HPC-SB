9377b9d32ade30049aa75b5c878e6193f2cad3ed
Jensen-holm <holmj@mail.gvsu.edu>
2023-10-27 11:35:40 -0400

    starting work on the matrix struct so we can test linear algebra operations across the different librarys we are learning

---
+++ Matrix/matrix.h
@@ -0,0 +1,11 @@
+
+struct matrix {
+    int height;
+    int width;
+};
+
+
+// mat_mul -> performs matrix multiplication 
+// between the two structs given as input
+matrix mat_mul(matrix a, matrix b) {
+}
--- OpenMp/main.c
+++ OpenMp/main.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 #include <omp.h>
+#include <matrix.h>
 
 
 int main(void) 
 {
+
+    struct matrix m1;
+    m1.width = 10;
+    m1.height = 10;
+
+    struct matrix m2;
+    m2.width = 10;
+    m2.height = 10;
+
     // set number of threads
     omp_set_num_threads(4);
 
-    // tell the compiler to create a 
-    // team of threads
+    // tell the compiler to create a team of threads
     #pragma omp parallel
     {
         int thread_id = omp_get_thread_num();
