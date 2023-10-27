ba24567288da056e1939c81496f97c3723539b6a
Jensen-holm <holmj@mail.gvsu.edu>
2023-10-27 10:25:31 -0400

    saying hello from different threads

--- OpenMp/main.c
+++ OpenMp/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+#include <omp.h>
+
 
 int main(void) 
 {
+    // set number of threads
+    omp_set_num_threads(4);
+
+    // tell the compiler to create a 
+    // team of threads
+    #pragma omp parallel
+    {
+        int thread_id = omp_get_thread_num();
+        printf("Hello from thread %d\n", thread_id);
+    }
 
     return 0;
 }
-
