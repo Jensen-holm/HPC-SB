fcf3c7c5d6903e88175b8e285623980a369675f5
Jensen-holm <holmj@mail.gvsu.edu>
2023-10-29 17:37:28 -0400

    initializing random matrix data with a max value, just wanted to make sure I was not using garbage values

--- OpenMp/openmp.c
+++ OpenMp/openmp.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <omp.h>
 #include "../Matrix/matrix.h"
 
 
 int main(void) 
 {
-    // set the seed for random numbers in matrix
-    srand(time(0));
-
     // set number of threads
     omp_set_num_threads(4);
 
-    struct matrix* mat1ptr = new_rand_matrix(5, 5);
+    struct matrix* mat1ptr = new_rand_matrix(5, 5, 10);
+    print_matrix(mat1ptr);
 
     // tell the compiler to create a team of threads
     #pragma omp parallel
@@ -21,14 +18,13 @@ int main(void)
         int thread_id = omp_get_thread_num();
         printf(" --- Thread %i ---\n", thread_id);
 
-        struct matrix* mat2ptr = new_rand_matrix(5, 5);
+        struct matrix* mat2ptr = new_rand_matrix(5, 5, 10);
         struct matrix* resultptr = new_empty_matrix(mat1ptr->rows, mat2ptr->cols);
         int is_valid = mat_mul(mat1ptr, mat2ptr, resultptr);
 
         if (is_valid == 1) {
             printf("condition not met for matrix multiplication.");
         } else {
-            print_matrix(resultptr);
         }
 
         free_matrix(mat2ptr);
