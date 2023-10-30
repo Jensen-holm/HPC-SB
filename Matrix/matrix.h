ead73416af377b028c4697774e0e3ff1a56725dd
Jensen-holm <holmj@mail.gvsu.edu>
2023-10-30 11:20:14 -0400

    I think that the matrix multiplication function is working

--- Matrix/matrix.h
+++ Matrix/matrix.h
@@ -2,63 +2,65 @@
 #include <stdio.h>
 #include <time.h>
 
-
-struct matrix 
+struct matrix
 {
     int rows;
     int cols;
     int data_points;
-    int* data;
+    float *data;
 };
 
-struct matrix* new_empty_matrix(int rows, int cols)
+struct matrix *new_empty_matrix(int rows, int cols)
 {
-    struct matrix* mat = (struct matrix*) malloc(sizeof(struct matrix));
+    struct matrix *mat = (struct matrix *)malloc(sizeof(struct matrix));
     mat->rows = rows;
     mat->cols = cols;
     mat->data_points = rows * cols;
-    mat->data = (int*) malloc(mat->data_points * sizeof(int));
+    mat->data = (float *)malloc(mat->data_points * sizeof(float));
 
     // fill all values with 0 to avoid garbage values
-    for (int i = 0; i < mat->data_points; i++) {
-        mat->data[i] = 0;
+    for (int i = 0; i < mat->data_points; i++)
+    {
+        mat->data[i] = 0.0;
     }
 
     return mat;
 }
 
-struct matrix* new_rand_matrix(int rows, int cols, int max)
+struct matrix *new_rand_matrix(int rows, int cols, int max)
 {
     srand(time(NULL));
-    struct matrix* mat = (struct matrix*) malloc(sizeof(struct matrix));
+    struct matrix *mat = (struct matrix *)malloc(sizeof(struct matrix));
     mat->rows = rows;
     mat->cols = cols;
     mat->data_points = rows * cols;
-    mat->data = (int*) malloc(mat->data_points * sizeof(int));
+    mat->data = (float *)malloc(mat->data_points * sizeof(float));
 
     // fill all values with 0 to avoid garbage values
-    for (int i = 0; i < mat->data_points; i++) {
+    for (int i = 0; i < mat->data_points; i++)
+    {
         mat->data[i] = rand() % max;
     }
 
     return mat;
 }
 
-void print_matrix(struct matrix* mat) 
+void print_matrix(struct matrix *mat)
 {
     printf("[\n");
-    for (int r = 0; r < mat->rows; r++) {
+    for (int r = 0; r < mat->rows; r++)
+    {
         printf("[");
-        for (int c = 0; c < mat->cols; c++) {
-            printf("%i, ", mat->data[r + c]);
+        for (int c = 0; c < mat->cols; c++)
+        {
+            printf("%f, ", mat->data[r + c]);
         }
         printf("]\n");
     }
     printf("]\n");
 }
 
-
-int mat_mul(struct matrix* m1, struct matrix* m2, struct matrix* result)
+int mat_mul(struct matrix *m1, struct matrix *m2, struct matrix *product)
 {
     // check if matrix multiplication pre reqs are met
     // if not return 1 (error), if it is met, do mat mul
@@ -67,20 +69,31 @@ int mat_mul(struct matrix* m1, struct matrix* m2, struct matrix* result)
     // number of cols from m2 should be the same as m1 rows
     int cols = m2->cols;
     int rows = m1->rows;
-    if (cols != rows) {
+    if (cols != rows)
+    {
         return 1;
     }
 
-    // assign the result matrix the correct dims
-    result->rows = rows;
-    result->cols = cols;
-    result->data = (int*) malloc(rows * cols * sizeof(int));
-
+    int cols1 = m1->cols;
+    int cols2 = m2->cols;
+    int rows1 = m1->rows;
+    for (int i = 0; i < rows1; i++)
+    {
+        for (int j = 0; j < cols2; j++)
+        {
+            float sum = 0.0;
+            for (int k = 0; k < cols1; k++)
+            {
+                sum += m1->data[i * cols1 + k] * m2->data[k * cols2 + j];
+            }
+            product->data[i * cols2 + j] = sum;
+        }
+    }
     return 0;
 }
 
-
-void free_matrix(struct matrix* mat) {
-    free(mat);
+void free_matrix(struct matrix *mat)
+{
     free(mat->data);
+    free(mat);
 }
--- OpenMp/openmp.c
+++ OpenMp/openmp.c
@@ -2,35 +2,37 @@
 #include <stdlib.h>
 #include <omp.h>
 #include "../Matrix/matrix.h"
+#include <time.h>
 
-
-int main(void) 
+int main(void)
 {
-    // set number of threads
-    omp_set_num_threads(4);
+    int MAX = 100;
+    srand(time(NULL));
+    omp_set_num_threads(100);
+    struct matrix *results_arr[100];
 
-    struct matrix* mat1ptr = new_rand_matrix(5, 5, 10);
-    print_matrix(mat1ptr);
-
-    // tell the compiler to create a team of threads
-    #pragma omp parallel
+// tell the compiler to create a team of threads
+#pragma omp parallel
     {
         int thread_id = omp_get_thread_num();
-        printf(" --- Thread %i ---\n", thread_id);
 
-        struct matrix* mat2ptr = new_rand_matrix(5, 5, 10);
-        struct matrix* resultptr = new_empty_matrix(mat1ptr->rows, mat2ptr->cols);
-        int is_valid = mat_mul(mat1ptr, mat2ptr, resultptr);
+        int dims = rand() % MAX;
+        struct matrix *mat1 = new_rand_matrix(dims, dims, MAX);
+        struct matrix *mat2 = new_rand_matrix(dims, dims, MAX);
 
-        if (is_valid == 1) {
-            printf("condition not met for matrix multiplication.");
-        } else {
-        }
+        struct matrix *product = new_empty_matrix(dims, dims);
+        int condition_met = mat_mul(mat1, mat2, product);
 
-        free_matrix(mat2ptr);
-        free_matrix(resultptr);
+        results_arr[thread_id] = product;
+        free_matrix(mat1);
+        free_matrix(mat2);
     }
 
-    free_matrix(mat1ptr);
+    for (int i = 0; i < 100; i++)
+    {
+        print_matrix(results_arr[i]);
+        free_matrix(results_arr[i]);
+    }
+
     return 0;
 }
