dbcb50f30c94add4db6bc27ce9f9409400cd8c30
Jensen-holm <holmj@mail.gvsu.edu>
2023-10-31 19:08:09 -0400

    parallel mat mul tested and it is STUPID fast

--- Matrix/test.c
+++ Matrix/test.c
@@ -6,15 +6,11 @@
 int main(void) {
     Matrix *mat1 = new_matrix(3, 3, 10);
     Matrix *mat2 = new_matrix(3, 3, 10);
-
-    clock_t begin = clock();
     Matrix *product = mat_mul(mat1, mat2);
-    clock_t end = clock();
 
-    double compute_time = (double) (begin - end) / CLOCKS_PER_SEC;
-    printf("compute time: %f", compute_time);
+    Matrix *product_parallel = mat_mul_parallel(mat1, mat2);
 
-    printf(" --- Matrix Multiplication ---\n");
+    printf(" --- Matrix Multiplication Naive ---\n");
     print_matrix(mat1);
     printf(" X \n");
     print_matrix(mat2);
@@ -22,8 +18,17 @@ int main(void) {
     print_matrix(product);
     printf("\n");
 
+    printf(" --- Matrix Multiplication Parallel ---\n");
+    print_matrix(mat1);
+    printf(" X \n");
+    print_matrix(mat2);
+    printf(" = \n");
+    print_matrix(product_parallel);
+    printf("\n");
+
     free_matrix(mat1);
     free_matrix(mat2);
     free_matrix(product);
+    free_matrix(product_parallel);
     return 0;
 }
