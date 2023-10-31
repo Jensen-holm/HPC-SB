2fe6acb601fcbaa027588cd5e591340104a14ee1
Jensen-holm <holmj@mail.gvsu.edu>
2023-10-31 08:35:24 -0400

    benchmark.sh running files given number of operations

--- benchmark.sh
+++ benchmark.sh
@@ -1,4 +1,12 @@
 #! /bin/bash
 
 num_operations=$1
-# run each hpc benchmark with the num operations arg and print the results
+
+echo "\ncomputing naive mat mul ..."
+naive_compute_time=$(./naive $num_operations)
+echo "naive runtime: ${naive_compute_time} seconds"
+
+echo "\ncomputing parallel mat mul ..."
+parallel_compute_time=$(./omp $num_operations)
+echo "parallel runtime: ${parallel_compute_time}"
+
